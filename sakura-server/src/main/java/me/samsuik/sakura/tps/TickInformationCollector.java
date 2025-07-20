5e5a3d1fe1454e6942c9cb6f0a7f6db0287c8be3
Samsuik <kfian294ma4@gmail.com>
2025-07-20 23:38:18 +0100

    uncomment entity count tracking
    This was commented during the 1.21.5 update while waiting for moonrise.

--- sakura-server/src/main/java/me/samsuik/sakura/tps/TickInformationCollector.java
+++ sakura-server/src/main/java/me/samsuik/sakura/tps/TickInformationCollector.java
@@ -26,8 +26,7 @@ public final class TickInformationCollector {
         int entities = 0;
         for (ServerLevel level : levels) {
             chunks += level.chunkSource.getFullChunksCount();
-            // todo 1.21.5 update
-            //entities += level.entityTickList.entities.size();
+            entities += level.entityTickList.entities.size();
         }
 
         double averageTick = this.tickSamples.longStream()
