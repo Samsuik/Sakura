95278079e2974043cf88498565944b6e91aa25d0
Samsuik <kfian294ma4@gmail.com>
2025-08-24 23:33:07 +0100

    prevent placing spawn eggs inside blocks

--- sakura-server/src/main/java/me/samsuik/sakura/configuration/WorldConfiguration.java
+++ sakura-server/src/main/java/me/samsuik/sakura/configuration/WorldConfiguration.java
@@ -186,9 +186,10 @@ public final class WorldConfiguration extends ConfigurationPart {
             public double shieldHitKnockback = 0.5;
         }
 
-        @Comment("Prevents players swimming using elytra or riptide to enter holes")
+        @Comment("Prevents players swimming, gliding or using riptide to enter small holes")
         public boolean posesShrinkCollisionBox = true;
         public boolean fishingHooksPullEntities = true;
+        public boolean preventPlacingSpawnEggsInsideBlocks = false;
     }
 
     public Entity entity;
