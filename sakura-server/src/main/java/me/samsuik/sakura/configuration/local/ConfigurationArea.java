1829714575d89e5f8f2a198051f970bf0f09c459
Samsuik <kfian294ma4@gmail.com>
2025-10-29 21:11:52 +0000

    make the ConfigurationArea contains inclusive

--- sakura-server/src/main/java/me/samsuik/sakura/configuration/local/ConfigurationArea.java
+++ sakura-server/src/main/java/me/samsuik/sakura/configuration/local/ConfigurationArea.java
@@ -24,9 +24,9 @@ public record ConfigurationArea(int minX, int minY, int minZ, int maxX, int maxY
     }
 
     public boolean contains(final int x, final int y, final int z) {
-        return x >= this.minX && x < this.maxX
-            && y >= this.minY && y < this.maxY
-            && z >= this.minZ && z < this.maxZ;
+        return x >= this.minX && x <= this.maxX
+            && y >= this.minY && y <= this.maxY
+            && z >= this.minZ && z <= this.maxZ;
     }
 
     public long volume() {
