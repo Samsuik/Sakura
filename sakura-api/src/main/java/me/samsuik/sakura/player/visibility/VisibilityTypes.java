d045731db398eacbe9a43009e56a95eaa4b3fdfe
Samsuik <kfian294ma4@gmail.com>
2026-05-09 18:58:54 +0100

    Add wind charges to visibility settings

--- sakura-api/src/main/java/me/samsuik/sakura/player/visibility/VisibilityTypes.java
+++ sakura-api/src/main/java/me/samsuik/sakura/player/visibility/VisibilityTypes.java
@@ -11,6 +11,7 @@ public final class VisibilityTypes {
     private static final List<VisibilityType> TYPES = new ArrayList<>();
 
     public static final VisibilityType TNT = create("tnt", true);
+    public static final VisibilityType WIND_CHARGE = create("wind_charge", true);
     public static final VisibilityType SAND = create("sand", true);
     public static final VisibilityType EXPLOSIONS = create("explosions", true);
     public static final VisibilityType SPAWNERS = create("spawners", false);
